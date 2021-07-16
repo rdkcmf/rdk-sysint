bf22e9eb7f77368e39b7b545d7d4720abe9788ea
Michael Amal Anand <Michael_AmalAnand@comcast.com>
2021-07-16 18:48:45 +0000

    DELIA-51420 Reboot reason is set as FIRMWARE FAILED during MaintenanceReboot
    Reason for change: Update the source of the reboot request
    Test Procedure: build procedure
    Risks:None
    
    Signed-off-by: Michael Anand Michael_AmalAnand@comcast.com
    (cherry picked from commit 4008c92b8c4acebefcb54a8a47ba60751eb61cb7)
    Change-Id: I8943fab809b704105683b24467b419dcc1426b42

--- lib/rdk/rebootNow.sh
+++ lib/rdk/rebootNow.sh
@@ -52,7 +52,7 @@ LOG_FILE=/opt/logs/messages.txt
 REBOOTINFO_LOGFILE=/opt/logs/rebootInfo.log
 
 # Define Reasons for APP_TRIGGERED, OPS_TRIGGERED and MAINTENANCE_TRIGGERED cases 
-APP_TRIGGERED_REASONS=(Servicemanager systemservice_legacy WarehouseReset WarehouseService HrvInitWHReset HrvColdInitReset HtmlDiagnostics InstallTDK StartTDK TR69Agent)
+APP_TRIGGERED_REASONS=(Servicemanager systemservice_legacy WarehouseReset WarehouseService HrvInitWHReset HrvColdInitReset HtmlDiagnostics InstallTDK StartTDK TR69Agent SystemServices)
 OPS_TRIGGERED_REASONS=(ScheduledReboot RebootSTB.sh FactoryReset UpgradeReboot_firmwareDwnld.sh UpgradeReboot_restore XFS wait_for_pci0_ready websocketproxyinit NSC_IR_EventReboot host_interface_dma_bus_wait usbhotplug Receiver_MDVRSet Receiver_VidiPath_Enabled Receiver_Toggle_Optimus S04init_ticket Network-Service monitor.sh ecmIpMonitor.sh monitorMfrMgr.sh vlAPI_Caller_Upgrade ImageUpgrade_rmf_osal ImageUpgrade_mfr_api ImageUpgrade_updateNewImage.sh ImageUpgrade_userInitiatedFWDnld.sh ClearSICache tr69hostIfReset hostIf_utils hostifDeviceInfo HAL_SYS_Reboot UpgradeReboot_deviceInitiatedFWDnld.sh UpgradeReboot_ipdnl.sh PowerMgr_Powerreset PowerMgr_coldFactoryReset DeepSleepMgr PowerMgr_CustomerReset PowerMgr_PersonalityReset Power_Thermmgr PowerMgr_Plat HAL_CDL_notify_mgr_event vldsg_estb_poll_ecm_operational_state BcmIndicateEcmReset SASWatchDog BP3_Provisioning)
 MAINTENANCE_TRIGGERED_REASONS=(AutoReboot.sh)
 
@@ -187,6 +187,9 @@ fi
 # Assign rebootreason from source category.
 if [[ "${APP_TRIGGERED_REASONS[@]}" == *"$source"* ]];then
     rebootReason="APP_TRIGGERED"
+    if [ $customReason == "MAINTENANCE_REBOOT" ];then
+         rebootReason="MAINTENANCE_REBOOT"
+    fi
 elif [[ "${OPS_TRIGGERED_REASONS[@]}" == *"$source"* ]];then
     rebootReason="OPS_TRIGGERED"
 elif [[ "${MAINTENANCE_TRIGGERED_REASONS[@]}" == *"$source"* ]];then
