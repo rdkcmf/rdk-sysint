ce9e9240047f8d3db711c8dc5375e61f1db2b12a
Anoop Ravi <Anoop_Ravi2@comcast.com>
2021-08-03 10:27:23 +0000

    RDKTV-9633 : Adding timestamp for networkInfoLogger.sh
    Reason for change: No Timestamp is logged in ipSetupLogs.txt
    Test Procedure: verify log file ipSetupLogs.txt
    Risks: Low
    Signed-off-by: Anoop Ravi <Anoop_Ravi2@comcast.com>
    
    (cherry picked from commit 9eb06d31f41fec0e23caf13e73cc497898df6553)
    Change-Id: I9ee753ac1d7de3376f1583e2adad3f0117d6fefe

--- lib/rdk/networkInfoLogger.sh
+++ lib/rdk/networkInfoLogger.sh
@@ -35,15 +35,15 @@ LOG_FILE=$LOG_PATH/$LOG_INPUT
 # Input Arguments - $1 event - $2 ipaddress type - $3 interface name - $4 ipaddress - $5 ipaddress scope  
 cmd=$1
 flags=$5
-printf "$0: Input Parameters : $* \n" >> $LOG_FILE
+printf "`/bin/timestamp` $0: Input Parameters : $* \n" >> $LOG_FILE
 if [ "x$cmd" == "xadd" ] && [ "x$flags" == "xglobal" ]; then
     # Debug Logs regarding the Network Informations
-    printf "$0: IP Informations\n `ifconfig` \n" >> $LOG_FILE
+    printf "`/bin/timestamp` $0: IP Informations\n `ifconfig` \n" >> $LOG_FILE
     wifiMac=`grep 'wifi_mac=' /tmp/.deviceDetails.cache | sed -e "s/wifi_mac=//g"`
     t2ValNotify "Xi_wifiMAC_split" "$wifiMac"
-    printf "$0: Route Informations\n `route -n` \n" >> $LOG_FILE
-    printf "$0: DNS Servers Informations" >> $LOG_FILE
-    printf "$0: DNS Masq File: /etc/resolv.dnsmasq\n `cat /etc/resolv.dnsmasq`\n" >> $LOG_FILE
-    printf "$0: DNS Resolve: /etc/resolv.conf\n `cat /etc/resolv.conf`\n" >> $LOG_FILE
+    printf "`/bin/timestamp` $0: Route Informations\n `route -n` \n" >> $LOG_FILE
+    printf "`/bin/timestamp` $0: DNS Servers Informations" >> $LOG_FILE
+    printf "`/bin/timestamp` $0: DNS Masq File: /etc/resolv.dnsmasq\n `cat /etc/resolv.dnsmasq`\n" >> $LOG_FILE
+    printf "`/bin/timestamp` $0: DNS Resolve: /etc/resolv.conf\n `cat /etc/resolv.conf`\n" >> $LOG_FILE
 fi
 exit 0
