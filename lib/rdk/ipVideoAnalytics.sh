768634d9cea910e36c1cd7074af4e8e98695551d
Sohail Ahmad <sohail_ahmad@comcast.com>
2022-01-31 05:51:41 +0000

    RDK-35210: Use XPKI Cert for MTLS
    Reason for change: Replacing RDK-CA with XPKI
    Test Procedure: Check if curl was successful for Video Analytics.
    Risks: Medium
    Signed-off-by: sohail_ahmad@cable.comcast.com
    
    (cherry picked from commit d76b05515d87582ca8a1be7cf69c2fb91f907d04)
    Change-Id: I0c805dcdfec62091b0537989df693e86b4089dad

--- lib/rdk/ipVideoAnalytics.sh
+++ lib/rdk/ipVideoAnalytics.sh
@@ -22,8 +22,6 @@
 
 . /etc/include.properties
 logsFile=$LOG_PATH/ipVideoAnalytics.log
-CERT_PATH="/etc/ssl/certs/rdkv-va-cpe-clnt.xcal.tv.cert.pem"
-CONFIG_PATH="/tmp/atdjivoqkkci"
 
 echo "`/bin/timestamp` IP video analytics " >> $logsFile
 
@@ -35,13 +33,10 @@ fi
 echo "[MTLS_VA] XB_LANIP : $XB_LANIP" >> $logsFile
 echo -e $XB_LANIP va-cpe-srvr.xcal.tv >> /etc/hosts
 
-if [ -d /etc/ssl/certs ]; then
-        if [ ! -f /usr/bin/GetConfigFile ];then
-                echo "Error: GetConfigFile Not Found" >> $logsFile
-                exit 127
-        fi
-        GetConfigFile $CONFIG_PATH
+if [ -f $RDK_PATH/mtlsUtils.sh ]; then
+    . $RDK_PATH/mtlsUtils.sh
 fi
+CERT=`getMtlsCreds ipVideoAnalytics.sh`
 
 if [ -f "/etc/webui/certs/comcast-rdk-ca-chain.cert.pem" ];then
         CA_FILE_PATH="/etc/webui/certs/comcast-rdk-ca-chain.cert.pem"
@@ -49,9 +44,11 @@ else
         CA_FILE_PATH="/etc/ssl/trmclient/comcast-rdk-ca-chain.cert.pem"
 fi
 
+echo "MTLS creds for Video Analytics fetched" >> $logsFile
 echo "[MTLS_VA] mTLS Video Analytics request" >> $logsFile
-CURL_CMD="curl -w '%{http_code}\n' --key $CONFIG_PATH --cert $CERT_PATH "https://va-cpe-srvr.xcal.tv:58081/test/OddEvenPattern.test" --cacert $CA_FILE_PATH -o /tmp/OddEvenPattern.test"
-HTTP_CODE=`curl -w '%{http_code}\n' --key $CONFIG_PATH --cert $CERT_PATH "https://va-cpe-srvr.xcal.tv:58081/test/OddEvenPattern.test" --cacert $CA_FILE_PATH -o /tmp/OddEvenPattern.test`
+
+CURL_CMD="curl -w '%{http_code}\n' $CERT "https://va-cpe-srvr.xcal.tv:58081/test/OddEvenPattern.test" --cacert $CA_FILE_PATH -o /tmp/OddEvenPattern.test"
+HTTP_CODE=`curl -w '%{http_code}\n' $CERT "https://va-cpe-srvr.xcal.tv:58081/test/OddEvenPattern.test" --cacert $CA_FILE_PATH -o /tmp/OddEvenPattern.test`
 http_code=$(echo "$HTTP_CODE" | awk -F\" '{print $1}' )
 echo "[MTLS_VA] CURL_CMD: $CURL_CMD" >> $logsFile
 echo "[MTLS_VA] http_code $http_code" >> $logsFile
@@ -60,10 +57,6 @@ if [ "$http_code" == 200 ]; then
         echo "[MTLS_VA] mTLS curl command successfull with http code: $http_code" >> $logsFile
 else
         echo "[MTLS_VA] mTLS curl command failed with http code: $http_code" >> $logsFile
-fi
-
-if [ -f $CONFIG_PATH ]; then
-        rm -rf $CONFIG_PATH
 fi
 
 echo "`/bin/timestamp` Exiting from IP video analytics " >> $logsFile
