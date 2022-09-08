3e9007720351bbf494ce66d1e1ca902437587536
gps496 <gomathishankar_ps@comcast.com>
2022-09-08 07:33:05 +0000

    DELIA-58019: copilot-cleanup service running before disk-init
    Reason for change: Modify copilot-cleanup service dependency to allign
    with apps-rdm service that downloads co-pilot and conecttv RDM Apps in
    /media/apps. Added Dependency with software update service.
    Test Procedure: Build and verify
    Risks: Low
    Priority: P0
    
    Signed-off-by: gps496 <gomathishankar_ps@comcast.com>
    (cherry picked from commit e2438735bc5d7d2716d848edde150cd466dd2c00)
    Change-Id: I9034228875bf67f00cadf3a650e2731818c6f504

--- lib/rdk/copilot-cleanup.sh
+++ lib/rdk/copilot-cleanup.sh
@@ -1,4 +1,4 @@
-#!bin/sh
+#!/bin/sh
 
 COPILOT_APP="/media/apps/co-pilot/"
 CONNECTTV_APP="/media/apps/connecttv/"
@@ -7,9 +7,23 @@ COPILOT_RDM="/media/apps/rdm/downloads/co-pilot/"
 CONNECTTV_RDM="/media/apps/rdm/downloads/connecttv/"
 
 # remove pre-installed Co-Pilot RDM Packages
-if [ -d "$COPILOT_APP" ]; then rm -rf $COPILOT_APP; fi
-if [ -d "$COPILOT_RDM" ]; then rm -rf $COPILOT_RDM; fi
+if [ -d "$COPILOT_APP" ]; then 
+    echo "CoPilot RDM App found in $COPILOT_APP Cleaning up"
+    rm -rf $COPILOT_APP;
+fi
+
+if [ -d "$COPILOT_RDM" ]; then
+    echo "CoPilot RDM Packages found in $COPILOT_RDM Cleaning up"
+    rm -rf $COPILOT_RDM;
+fi
 
 # remove pre-installed ConnectTV RDM Packages
-if [ -d "$CONNECTTV_APP" ]; then rm -rf $CONNECTTV_APP; fi
-if [ -d "$CONNECTTV_RDM" ]; then rm -rf $CONNECTTV_RDM; fi
+if [ -d "$CONNECTTV_APP" ]; then
+    echo "ConnectTV RDM App found in $CONNECTTV_APP Cleaning up"
+    rm -rf $CONNECTTV_APP;
+fi
+
+if [ -d "$CONNECTTV_RDM" ]; then
+    echo "ConnectTV RDM Packages found in $CONNECTTV_RDM Cleaning up"
+    rm -rf $CONNECTTV_RDM;
+fi
