d2c6dc7567bb9ad7d3b035def3a0494eb5a49243
vnarwa156 <VivekSingh_Narwaria@comcast.com>
2022-09-09 19:37:31 +0000

    RDKTV-18608:[performance improvements] clean-up and optimisation of StartIPgw.sh
    Reason for change: clean-up and optimisation of StartIPgw.sh
    Test Procedure: Build using Jenkins should pass
    Risks: None
    Signed-off-by: Vivek Narwaria (VivekSingh_Narwaria@comcast.com)
    
    (cherry picked from commit 47fd48b4eeb1d5a334a1b49d7d7c86ff6fa28d90)
    Change-Id: I4ba6ebee969c325d3dc942d635fe530a5f265e84

--- lib/rdk/startIpGw.sh
+++ lib/rdk/startIpGw.sh
@@ -17,128 +17,91 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 ##############################################################################
-
-
 . /etc/include.properties
 . $RDK_PATH/utils.sh
-. /etc/device.properties 
+. /etc/device.properties
 . /etc/env_setup.sh
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 logsFile=$LOG_PATH/ipSetupLogs.txt
 wifi_interface=`getWiFiInterface`
 interface=`getMoCAInterface`
+
+ipGWLog() {
+    echo "`date -u +%d.%m.%Y_%H:%M:%S`: $0: $*" >> $logsFile
+}
+
 ret=`checkWiFiModule`
 if [ $ret == 1 ]; then
-	echo "`/bin/timestamp` WIFI is enabled" >> $logsFile
-	interface=$wifi_interface
-fi	
+    ipGWLog "WIFI is enabled"
+    interface=$wifi_interface
+fi
 
-#:>$logsFile
+CheckIPConnection()
+{
+    dnsmasq=`cat /etc/resolv.dnsmasq`
+    PING_BIN="$1"
+    if [ "$dnsmasq" == "" ]; then
+        ipGWLog "DNS masq is not set Please check output.json for dns config ****  "
+    fi
+    $PING_BIN -c 3 $gatewayIP   > /dev/null 2>/dev/null
+    while [  $? -eq 0 ];
+    do
+        if [ ! -f /tmp/gatewayConnected ]; then
+            process=`ps | grep gwConnNotify  | grep -v grep `
+            if [ "$process" = "" ]; then
+                sh $RDK_PATH/gwConnNotify.sh &
+                ipGWLog "Connected to Gateway  $gatewayIP"
+            fi
+        fi
+        sleep 15
+        $PING_BIN -c 3 $gatewayIP  > /dev/null 2>/dev/null
+    done;
+}
 
-#if [ -f $PERSISTENT_PATH/moca_interface ] ; then
-#    interface=`cat $PERSISTENT_PATH/moca_interface`
-#else
-#    interface=eth1
-#fi
-
-while true; 
+while true;
 do
-	echo "`/bin/timestamp` starting the ipsetup and gateway setup "   >> $logsFile
-        [ ! -f /etc/os-release ]  && sh $RDK_PATH/ipSetup.sh
-	sleep 15
-	if [ ! -f /etc/os-release ]; then
-		# We dont need this script in yocto since by the
-		# time this script is done, moca ip is acquired.
-		sleep 1
-	fi
-	process=`ps | grep gwConnNotify.sh  | grep -v grep `
-	if [ "$process" != "" ]; then
-		ps  | grep gwConnNotify.sh | grep -v grep | awk '{print $1}'| xargs kill -9
-	fi
-	if [ -f /tmp/gatewayConnected ] && [ -f /usr/local/bin/IARM_event_sender ]; then
-		echo "`/bin/timestamp` sending gateway disconnected event" >> $logsFile
-		if [ ! -f /etc/os-release ]; then
-		    /usr/local/bin/IARM_event_sender GatewayConnEvent 0
-		    /usr/local/bin/IARM_event_sender MocaStatusEvent 1
-		else
-		    /usr/bin/IARM_event_sender GatewayConnEvent 0
-		    /usr/bin/IARM_event_sender MocaStatusEvent 1
-		fi
-		rm /tmp/gatewayConnected
-	fi	
-        if [ -f /tmp/estb_ipv4 ]; then    
-            gatewayIP=`route -n | grep $interface | grep 'UG[ \t]' | awk '{print $2}'`
-            gatewayIP=`route -n | grep 'UG[ \t]' | grep $interface | awk '{print $2}' | grep 169.254`
-            gatewayIP=`echo $gatewayIP | head -n1 | awk '{print $1;}'`
-            if [ "$KICKSTART" = "yes" ]; then
-                    gatewayIP=`route -n | grep 'UG[ \t]' | awk '{print $2}'`
-                    touch /tmp/gatewayConnected
-            fi
-            if [ "$gatewayIP" = "" ]; then
-                    echo "`/bin/timestamp` No gateway IP going to ipsetup "  >> $logsFile
+    ipGWLog "Starting the ipsetup and gateway setup "
+    [ ! -f /etc/os-release ]  && sh $RDK_PATH/ipSetup.sh
+        sleep 15
+        if [ ! -f /etc/os-release ]; then
+            # We dont need this script in yocto since by the time this script is done, moca ip is acquired.
+            sleep 1
+        fi
+        process=`ps | grep gwConnNotify.sh  | grep -v grep `
+        if [ "$process" != "" ]; then
+            ps  | grep gwConnNotify.sh | grep -v grep | awk '{print $1}'| xargs kill -9
+        fi
+        if [ -f /tmp/gatewayConnected ] && [ -f /usr/local/bin/IARM_event_sender ]; then
+            ipGWLog "sending gateway disconnected event"
+            if [ ! -f /etc/os-release ]; then
+                /usr/local/bin/IARM_event_sender GatewayConnEvent 0
+                /usr/local/bin/IARM_event_sender MocaStatusEvent 1
             else
-                    dnsmasq=`cat /etc/resolv.dnsmasq`
-                    if [ "$dnsmasq" == "" ]; then
-                            echo "`/bin/timestamp` DNS masq is not set Please check output.json for dns config ****  " >> $logsFile
-                    fi
-    #               sh /test.sh
-                    ping -c 3 $gatewayIP   > /dev/null 2>/dev/null
-                    while [  $? -eq 0 ];
-                    do
-                            if [ ! -f /tmp/gatewayConnected ]; then
-                                    process=`ps | grep gwConnNotify  | grep -v grep `
-                                    if [ "$process" = "" ]; then
-                                            sh $RDK_PATH/gwConnNotify.sh &
-                                            echo "`/bin/timestamp` Connected to Gateway  $gatewayIP  "  >> $logsFile
-                                    fi
-                            fi
-                            sleep 15
-                            ping -c 3 $gatewayIP    > /dev/null 2>/dev/null
-                    done;
-
+                /usr/bin/IARM_event_sender GatewayConnEvent 0
+                /usr/bin/IARM_event_sender MocaStatusEvent 1
             fi
-         elif [ -f /tmp/estb_ipv6 ]; then    
-            gatewayIP=`ip -6 route | grep $interface | awk '/default/ { print $3 }'`
-            if [ "$KICKSTART" = "yes" ]; then
-                    gatewayIP=`ip -6 route | awk '/default/ { print $3 }'`
-                    touch /tmp/gatewayConnected
-            fi
+            rm /tmp/gatewayConnected
+        fi
+        if [ -f /tmp/estb_ipv4 ]; then
+            gatewayIP=`route -n | grep 'UG[ \t]'`
+            gatewayIP=`echo $gatewayIP | head -n1 | grep $interface | awk '{print $2}' | grep 169.254`
+            PING="/bin/ping"
             if [ "$gatewayIP" = "" ]; then
-                    sleep 5
+                ipGWLog "No gateway IP going to ipsetup "
             else
-                    dnsmasq=`cat /etc/resolv.dnsmasq`
-                    if [ "$dnsmasq" == "" ]; then
-                            echo "`/bin/timestamp` DNS masq is not set Please check output.json for dns config ****  " >> $logsFile
-                    fi
-    #               sh /test.sh
-                    ping6 -c 3 $gatewayIP   > /dev/null 2>/dev/null
-                    while [  $? -eq 0 ];
-                    do
-                            if [ ! -f /tmp/gatewayConnected ]; then
-                                    process=`ps | grep gwConnNotify  | grep -v grep `
-                                    if [ "$process" = "" ]; then
-                                            sh $RDK_PATH/gwConnNotify.sh &
-                                            echo "`/bin/timestamp` Connected to Gateway  $gatewayIP  "  >> $logsFile
-                                    fi
-                            fi
-                            sleep 15
-                            ping6 -c 3 $gatewayIP    > /dev/null 2>/dev/null
-                    done;
-
+                CheckIPConnection $PING
+            fi
+        elif [ -f /tmp/estb_ipv6 ]; then
+            gatewayIP=`ip -6 route > /tmp/tmpIPv6RouteData`
+            gatewayIP=`cat /tmp/tmpIPv6RouteData | grep $interface | awk '/default/ { print $3 }'`
+            PING="/bin/ping6"
+            if [ "$gatewayIP" = "" ]; then
+                sleep 5
+            else
+                CheckIPConnection $PING
             fi
          else
-            echo "`/bin/timestamp` Box is not configured in IPv4 or IPv6 mode yet..Wait & Retry..!" >> $logsFile
+            ipGWLog "Box is not configured in IPv4 or IPv6 mode yet..Wait & Retry..!"
          fi
-
-#        if [ ! -f /tmp/rstXdiscovery ]; then
-#	    process=`ps | grep rstXdiscovery  | grep -v grep `
-#    	    if [ "$process" = "" ]; then
-#            	sh $RDK_PATH/rstXdiscovery.sh &
-#            fi
-#        fi
 done
-
-
-
-
-
